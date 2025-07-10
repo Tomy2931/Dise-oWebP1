b1d01de1731451b351f7c853dcf295783db74662
Tomy2931 <tomas.cejas@hotmail.com>
2025-07-10 16:44:44 -0300

    Agregando desplegable carrito

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-const iconCarrito = document.getElementById("iconoCarrito");
+const iconCarrito = document.getElementById("iconCarrito");
+const carritoInfo = document.getElementById("carritoInfo");
 
 iconCarrito.addEventListener("click", () => {
-    alert("¡Hola!");
-  });
+    if (carritoInfo.style.display == "none"){
+      carritoInfo.style.display = "block";
+      
+    } else {
+      
+      carritoInfo.style.display = "none";
+    }
+});
